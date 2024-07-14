b7f4811fe6453e68ae135af235fa92c0512e5219
Aroo <aroo25may@gmail.com>
2024-07-14 12:53:51 +0530

    14 JULY 2024

---
+++ 2. Medium/129. Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createBinaryTree(const vector<int>& values);
+void printBinaryTree(TreeNode* root);
+int sumNumbers(TreeNode* root);
+int dfs(TreeNode*, int);
+
+int main()
+{
+    vector<int> list = {4,9,0,5,1};
+    TreeNode* root = createBinaryTree(list);
+    int res;
+
+    res = sumNumbers(root);
+    cout<<res;
+
+   return 0;
+}
+
+int sumNumbers(TreeNode* root){
+    int sum = dfs(root, 0);
+    return sum;
+}
+
+int dfs(TreeNode* root, int sum){
+    if(root==NULL)
+        return 0;
+    
+    sum = sum * 10 + root->val;
+    if(root->left == NULL && root->right == NULL)
+        return sum;
+    
+    return dfs(root->left, sum) + dfs(root->right, sum);
+}
+
+// Helper Function to create a binary tree from a level-order input
+TreeNode* createBinaryTree(const vector<int>& values){
+    if (values.empty() || values[0] == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(values[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < values.size()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        if (values[i] != -1) {
+            current->left = new TreeNode(values[i]);
+            q.push(current->left);
+        }
+        i++;
+        if (i < values.size() && values[i] != -1) {
+            current->right = new TreeNode(values[i]);
+            q.push(current->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+// Helper Function to print the binary tree in level order
+void printBinaryTree(TreeNode* root){
+    if (!root) return;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+        if (current) {
+            cout << current->val << " ";
+            q.push(current->left);
+            q.push(current->right);
+        } else {
+            cout << "null ";
+        }
+    }
+    
+    cout << endl;
+}
