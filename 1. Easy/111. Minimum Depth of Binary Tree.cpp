a7cfa82509a5db77b83fae9aa49ba913a6866312
Aroo <aroo25may@gmail.com>
2024-07-15 20:43:17 +0530

    15 JULY 2024

---
+++ 1. Easy/111. Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* createBinaryTree(const vector<int>& values);
+void printBinaryTree(TreeNode* root);
+int minDepth(TreeNode* root);
+
+int main()
+{
+    vector<int> list = {3,9,20,-1,-1,15,7};
+    TreeNode* root = createBinaryTree(list); 
+    int min_depth = 0;
+
+    min_depth = minDepth(root);
+    cout<<min_depth;
+
+   return 0;
+}
+
+int minDepth(TreeNode* root){
+    if(root==NULL)
+        return 0;
+    
+    if(root->left == NULL)
+        return 1 + minDepth(root->right);
+
+    if(root->right == NULL)
+        return 1 + minDepth(root ->left);
+
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
+
+// Helper Function to create a binary tree from a level-order input
+TreeNode* createBinaryTree(const vector<int>& values){
+    if (values.empty() || values[0] == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(values[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < values.size()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        if (values[i] != -1) {
+            current->left = new TreeNode(values[i]);
+            q.push(current->left);
+        }
+        i++;
+        if (i < values.size() && values[i] != -1) {
+            current->right = new TreeNode(values[i]);
+            q.push(current->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+// Helper Function to print the binary tree in level order
+void printBinaryTree(TreeNode* root){
+    if (!root) return;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+        if (current) {
+            cout << current->val << " ";
+            q.push(current->left);
+            q.push(current->right);
+        } else {
+            cout << "null ";
+        }
+    }
+    
+    cout << endl;
+}
