d2f75ee658bb866be7269cdbf3cc72e152983c73
Aroo <aroo25may@gmail.com>
2024-10-24 00:18:10 +0530

    24 OCTOBER 2024

--- 2. Medium/80. Remove Duplicates from Sorted Array II.cpp
+++ 2. Medium/80. Remove Duplicates from Sorted Array II.cpp
@@ -6,22 +6,22 @@ int main()
 {
     vector<int> nums = {1,1,1,2,2,2,3,3,3};
     int l = 0;
-        int r = 0;
+    int r = 0;
         
-        while (r < nums.size()) {
-            int count = 1;
-            while (r + 1 < nums.size() && nums[r] == nums[r + 1]) {
-                r++;
-                count++;
-            }
-            
-            for (int i = 0; i < std::min(2, count); i++) {
-                nums[l] = nums[r];
-                l++;
-            }
-            
+    while (r < nums.size()) {
+        int count = 1;
+        while (r + 1 < nums.size() && nums[r] == nums[r + 1]) {
             r++;
+            count++;
         }
+        
+        for (int i = 0; i < std::min(2, count); i++) {
+            nums[l] = nums[r];
+            l++;
+        }
+        
+        r++;
+    }
 
     for(int i = 0; i<l; i++){
         cout<<nums[i]<<" ";
