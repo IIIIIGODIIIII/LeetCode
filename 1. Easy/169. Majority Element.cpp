42081a97d1659ee1d6456f89e3d82009a3d13204
Aditya <aroo25may@gmail.com>
2024-11-03 21:52:46 +0530

    3 NOVEMBER 2024

--- 1. Easy/169. Majority Element.cpp
+++ 1. Easy/169. Majority Element.cpp
@@ -2,20 +2,40 @@
 #include<bits/stdc++.h>
  
 using namespace std;
- 
-int main()
-{
-    vector<int> nums = {2,2,1,1,1,2,2};
-    map<int, int> m;
-    int result = 0;
 
-    for(int i = 0; i<nums.size(); i++){
-        m[nums[i]]++;
-        if(m[nums[i]] > nums.size()/2){
-            result = nums[i];
-            break;
-        }   
+/*int majorityElement(vector<int>& nums) {
+    unordered_map<int, int> mpp;
+
+    for(int i = 0; i < nums.size(); i++){
+        mpp[nums[i]] += 1;
+
+        if(mpp[nums[i]] > nums.size()/2)
+            return nums[i];
     }
-    cout<<result;
-   return 0;
+
+    return -1;
+}*/
+
+// OR
+
+int majorityElement(vector<int>& nums) {
+    int count = 0;
+    int candidate = 0;
+
+    for(auto num : nums){
+        if(count == 0)
+            candidate = num;
+        
+        count += (num == candidate) ? 1 : -1;
+    }
+
+    return candidate;
+}
+
+int main() {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    
+    cout<<majorityElement(nums);
+
+    return 0;
 }
