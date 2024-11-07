2f761a19a0f75ba16ad106a037114086d7751b1c
Aditya <aroo25may@gmail.com>
2024-11-07 23:02:04 +0530

    7 NOVEMBER 2024

--- 2. Medium/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
+++ 2. Medium/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -4,11 +4,24 @@
 using namespace std;
 
 int largestCombination(vector<int>& candidates) {
-    
+    int largest = 0;
+
+    for(int i = 0; i < 24; i++){
+        int count = 0;
+
+        for(auto candidate : candidates){
+            if(candidate >> i & 1)
+                count += 1;
+        }
+
+        largest = max(largest, count);
+    }
+
+    return largest;
 }
 
 int main() {
-    vector<int> candidates = {16,17,71,62,12,24,14};
+    vector<int> candidates = {33,93,31,99,74,37,3,4,2,94,77,10,75,54,24,95,65,100,41,82,35,65,38,49,85,72,67,21,20,31};
 
     cout<<largestCombination(candidates);
 
