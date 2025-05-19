db1b99f7f382825ddacfdb9fff8ec98902798396
Aditya <aroo25may@gmail.com>
2025-05-19 22:57:07 +0530

    19 MAY 2025

---
+++ 1. Easy/3024. Type of Triangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+string triangleType(vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+
+    if (nums[0] + nums[1] <= nums[2])
+      return "none";
+
+    if (nums[0] == nums[1] && nums[1] == nums[2])
+      return "equilateral";
+
+    if (nums[0] == nums[1] || nums[1] == nums[2])
+      return "isosceles";
+
+    return "scalene";
+}
+
+int main() {
+    vector<int> nums = {3,3,3};
+    
+    cout<<triangleType(nums);
+
+    return 0;
+}
---
+++ 3. Hard/1931. Painting a Grid With Three Different Colors.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static constexpr int kMod = 1'000'000'007;
+int global_m;
+int global_n;
+vector<vector<int>> mem = vector<vector<int>>(1000, vector<int>(1024));
+
+int dp(int r, int c, int prevColMask, int currColMask) {
+    if (c == global_n)
+        return 1;
+
+    if (mem[c][prevColMask])
+        return mem[c][prevColMask];
+
+    if (r == global_m)
+        return dp(0, c + 1, currColMask, 0);
+
+    int ans = 0;
+
+    // 1 := red, 2 := green, 3 := blue
+    for (int color = 1; color <= 3; ++color) {
+        if (getColor(prevColMask, r) == color)
+            continue;
+        if (r > 0 && getColor(currColMask, r - 1) == color)
+            continue;
+        ans += dp(r + 1, c, prevColMask, setColor(currColMask, r, color));
+        ans %= kMod;
+    }
+
+    if (r == 0)
+        mem[c][prevColMask] = ans;
+
+    return ans;
+}
+
+// e.g. __ __ __ __ __
+//      01 10 11 11 11
+//      R  G  B  B  B
+// getColor(0110111111, 3) -> G
+int getColor(int mask, int r) {
+    return mask >> r * 2 & 3;
+}
+
+int setColor(int mask, int r, int color) {
+    return mask | color << r * 2;
+}
+
+int colorTheGrid(int m, int n) {
+    global_m = m;
+    global_n = n;
+    return dp(0, 0, 0, 0);
+}
+
+int main() {
+    int m = 1, n = 1;
+
+    cout << colorTheGrid(m, n);
+
+    return 0;
+}
