e243107a6bba2a30f2b0feb7572659f136cdebdb
Aditya <aroo25may@gmail.com>
2025-05-24 13:18:05 +0530

    24 MAY 2025

---
+++ 3. Hard/3068. Find the Maximum Sum of Node Values.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+
+long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+    long maxSum = 0;
+    int changedCount = 0;
+    int minChangeDiff = INT_MAX;
+
+    for (const int num : nums) {
+      maxSum += max(num, num ^ k);
+      changedCount += ((num ^ k) > num) ? 1 : 0;
+      minChangeDiff = min(minChangeDiff, abs(num - (num ^ k)));
+    }
+
+    if (changedCount % 2 == 0)
+      return maxSum;
+      
+    return maxSum - minChangeDiff; 
+}
+
+int main() {
+    vector<int> nums = {1,2,1};
+    int k = 3;
+    vector<vector<int>> edges = {{0,1}, {0,2}};
+    
+    cout<<maximumValueSum(nums, k, edges);
+
+    return 0;
+}
